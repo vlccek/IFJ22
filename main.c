ce7b80e41f20ad4c5967ac8e0bdb7824577ed2e2
Jakub Vlk <kuba24862vlk@gmail.com>
2022-09-20 12:15:31 +0200

    add main file

---
+++ main.c
@@ -0,0 +1,27 @@
+//
+// Created by jvlk on 20.9.22.
+//
+
+#include "queue.c"
+
+int main(int argc, char *argv[]) {
+
+    stack_t *st = stack();
+
+    struct ints2 a = {10, 20};
+    struct ints2 b = {11, 20};
+    struct ints2 c = {10, 21};
+
+    enqueue(st, a);
+    push(st, b);
+    push(st, c);
+    printStack(st, printMember);
+
+
+    typeOfStoredData tmp = popBack(st);
+    //  freeStackEl(tmp);
+
+
+    return 0;
+
+}
