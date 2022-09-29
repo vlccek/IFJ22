2b006026defdcc52163c484a21d5c7a02d11d210
Jindřich Vodák <jindrich.vodak@email.cz>
2022-09-29 19:46:43 +0200

    printTokenData() update

--- lex.c
+++ lex.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include "lex.h"
 
+// prints the content of a token
 void printTokenData(token_t input)
 {
     switch (input.type)
@@ -18,9 +19,66 @@ void printTokenData(token_t input)
         fprintf(stderr, "lextype: stringLiteral\n");
         fprintf(stderr, "value: %s\n", dstrGet(input.data.valueString));
         break;
+    case integerLiteral:
+        fprintf(stderr, "lextype: integerLiteral\n");
+        fprintf(stderr, "value: %d\n", input.data.valueInteger);
+        break;
+    case decimalLiteral:
+        fprintf(stderr, "lextype: decimalLiteral\n");
+        fprintf(stderr, "value: %lf\n", input.data.valueNumber);
+        break;
+    // keywords
+    case elseKey:
+    case functionKey:
+    case ifKey:
+    case nullKey:
+    case returnKey:
+    case voidKey:
+    case whileKey:
+    // curly braces
+    case curlyBraceRight:
+    case curlyBraceLeft:
+    // data types
+    case stringDat:
+    case floatDat:
+    case intDat:
+    case stringNullDat:
+    case floatNullDat:
+    case intNullDat:
+    // assignment
+    case equals:
+    case colon:
+    case semiColon:
+    case comma:
+    // operators
+    case multiplicationOp:
+    case divisionOp:
+    case plusOp:
+    case minusOp:
+    case concatenationOp:
+    case lesserThanOp:
+    case lesserEqOp:
+    case greaterThanOp:
+    case greaterEqOp:
+    case eqOp:
+    case notEqOp:
+    // parentheses
+    case leftPar:
+    case rightPar:
+    // identifiers
+    case identifierFunc:
+    case identifierVar:
+    // ending
+    case ending:
+    // unknown & default
+    case unknown:
     default:
-        // TODO
+        fprintf(stderr, "lextype: unknown\n");
         break;
     return;
     }
+
+    fprintf(stderr, "row: %d\nrow position: %d\n\n", input.rowNumber, input.rowPosNumber);
+
+    return;
 }
