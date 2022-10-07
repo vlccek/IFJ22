5ab89db9085668f82723bb1a203b8a10628d7c47
Jakub Vlk <kuba24862vlk@gmail.com>
2022-10-07 20:42:38 +0200

    rework parsing file

--- parse.py
+++ parse.py
@@ -1,51 +1,25 @@
-from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import requests
 
 with open("LL(1) Parser Generator.html") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
-    table = soup.find_all("table", {'class': 'pure-table pure-table-bordered'})[1]
-    header_top = [i.text for i in table.find_all("th")][1::]
-    header_left = [i.select("td") for i in table.select("#llTableRows > tr")]
-    left = []
-    for i in header_left:
-        for l in i:
-            if ( not ("::=" in l.text) and l.text != ""):
-                left.append(l.text)
-
-    header_left = left
-
-    rows = soup.find_all("table", {'class': 'pure-table pure-table-bordered'})[1].find("tbody").find_all("tr")
-
-    sloupec = 0
-    for row in rows:
-        cells = row.find_all("td")[1::]
-        try:
-            for i in range(len(cells)):
-                x = header_left[sloupec].replace("$", "ending")
-                y = header_top[i].replace("$", "ending")
-                mc = cells[i].text
-                if mc != "":
-                    pravidla = mc.split("::=")[1].split()
-                    delka = len(pravidla)
-                    if "ε" in pravidla:
-                        print(f""" insertMember({x}, {y}, 0 )""", end="\n")
-                        continue
-                    print(f""" insertMember({x}, {y}, {delka}, """, end="")
-                    for pr in pravidla:
-                        if pr == "$":
-                            pr = "ending"
-                        elif pr == "ε":
-                            continue
-                        print(f"""partOfRulesRightSide({pr})""", end="")
-                        if (pr == pravidla[-1] or pr == "ending"):
-                            print("", end="")
-                        else:
-                            print("," , end="")
-                    print(");")
-        except:
-            pass
-        sloupec+=1
-
-
-
-
-
+    df_list = pd.read_html(fp, encoding='ISO-8859-1')  # this parses all the tables in webpages to a list
+    df = df_list[0]
+    df.head()
+    for index, row in df.iterrows():
+        for i in df.head():
+            r = row[i]
+            if not (pd.isnull(r)):
+                x = i.replace("$", "ending")
+                try:
+                    y = r.split("::=")[0]
+                    rule = r.split("::=")[1].split()
+                    if (rule == ["ε"]):
+                        print(f"insertMember({x}, {y}, 0)")
+                    print(f"insertMember({x}, {y}, {len(rule)}", end="")
+                    for l in rule:
+                        l = l.replace("ε","eps").replace("$", "ending")                
+                        print(f", {l}" ,end="")
+                    print(")")
+                except:
+                    pass
