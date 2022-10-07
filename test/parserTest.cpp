3a31742afeceed48fe030b028500472dad030fb9
Jakub Vlk <kuba24862vlk@gmail.com>
2022-10-07 23:29:27 +0200

    add auto data generation in tokens

--- test/parserTest.cpp
+++ test/parserTest.cpp
@@ -10,6 +10,7 @@ extern "C" {
 #include "../parser.h"
 #include "../parser.c"
 // DON'T TOUCH THIS!
+#include "../dynstring.h"
 
 #include <stdio.h>
 };
@@ -29,10 +30,12 @@ namespace ifj22 {
     namespace parsertest {
         class ParserTest : public ::testing::Test {
         protected:
-
             std::vector<token_t> *tokens;
 
-            void getAllTokens() {
+            int uID = 0
+
+                    void
+                    getAllTokens() {
                 return;
             }
 
@@ -43,6 +46,21 @@ namespace ifj22 {
             void tokensForParser(const std::vector<lexType> &lexTypes) {
                 for (auto lexType: lexTypes) {
                     token_t newToken = {lexType, {}, 0, 0};
+                    switch (lexType) {
+                        case integerLiteral:
+                            newToken.data.valueInteger = 10;
+                            break;
+                        case floatLiteral:
+                            newToken.data.valueFloat = 10;
+                            break;
+                        case stringLiteral:
+                            newToken.data.valueString = dstrInitChar("hovnokod");
+                            break;
+                        case identifierFunc:
+                        case identifierVar:
+                            newToken.data.valueString = dstrInitChar(std::string("hovnokod") + std::to_string(uID));
+                            break;
+                    }
                     tokens->push_back(newToken);
                 }
 
@@ -76,20 +94,65 @@ namespace ifj22 {
             ASSERT_EXIT_SYNTAX(parser());
         }
 
-        // region operation_concat
-        TEST_F(ParserTestSyntaxError, concatenationOp_basic) {
+        // region basic_op
+        TEST_F(ParserTestSuccess, concatenationOp_basic) {
             tokensForParser({stringLiteral, concatenationOp, identifierVar, ending});
 
-            ASSERT_EXIT_SYNTAX(parser());
+            parser();
         }
+        TEST_F(ParserTestSuccess, plusOP) {
+            tokensForParser({stringLiteral, plusOp, identifierVar, ending});
 
+            parser();
+        }
+        TEST_F(ParserTestSuccess, minusOP) {
+            tokensForParser({stringLiteral, minusOp, identifierVar, ending});
+
+            parser();
+        }
+        TEST_F(ParserTestSuccess, multipl) {
+            tokensForParser({stringLiteral, multiplicationOp, identifierVar, ending});
+
+            parser();
+        }
+        TEST_F(ParserTestSuccess, div) {
+            tokensForParser({stringLiteral, divisionOp, identifierVar, ending});
+
+            parser();
+        }
+        // endregion
+
+        // region pars_op
         TEST_F(ParserTestSyntaxError, concatenationOp_par) {
             tokensForParser(
                     {stringLiteral, concatenationOp, leftPar, identifierVar, concatenationOp, identifierVar, rightPar,
                      ending});
 
-            ASSERT_EXIT_SYNTAX(parser());
+            parser();
         }
+        TEST_F(ParserTestSyntaxError, add_par) {
+            tokensForParser(
+                    {stringLiteral, plusOp, leftPar, identifierVar, plusOp, identifierVar, rightPar,
+                     ending});
+
+            parser();
+        }
+        TEST_F(ParserTestSyntaxError, multiOp_par) {
+            tokensForParser(
+                    {stringLiteral, multiplicationOp, leftPar, identifierVar, multiplicationOp, identifierVar, rightPar,
+                     ending});
+
+            parser();
+        }
+        TEST_F(ParserTestSyntaxError, divOP_par) {
+            tokensForParser(
+                    {stringLiteral, divisionOp, leftPar, identifierVar, divisionOp, identifierVar, rightPar,
+                     ending});
+
+            parser();
+        }
+
+        //end region
 
 
         TEST_F(ParserTestSuccess, functionDeclaration) {
@@ -99,6 +162,8 @@ namespace ifj22 {
 
             parser();
         }
+
+        //endregion
 
         TEST_F(ParserTestSuccess, functionDeclaration_returnVoid) {
             tokensForParser(
