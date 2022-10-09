462bcf3d5701d3cd0decd346679945cbf0418604
Jakub Vlk <kuba24862vlk@gmail.com>
2022-10-09 16:36:00 +0200

    get n elemnt from stack + tests

--- stack.c
+++ stack.c
@@ -140,3 +140,17 @@ void *getFromTop(genericStack *s, unsigned int numberForTop) {
 
     return i->data;
 }
+/*
+ * Returns null when elemnt not exist
+ */
+void *getFromTop(genericStack *s, unsigned int numberForTop) {
+    stackMem_t *i = s->top;
+    while (numberForTop != 0 && s->c > numberForTop--) {
+        i = i->next;
+    }
+    if (numberForTop != 0) {
+        return NULL;
+    }
+
+    return i->data;
+}
