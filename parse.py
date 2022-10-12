443760e22b0f0c0073b8accc35c39d1ec6c97d57
Jakub Vlk <kuba24862vlk@gmail.com>
2022-10-12 18:37:27 +0200

    fix more expresion rules

--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import requests
 
-with open("LLtable.html") as fp:
+with open("LL(1) Parser Generator.html") as fp:
     df_list = pd.read_html(fp, encoding='ISO-8859-1')  # this parses all the tables in webpages to a list
     df = df_list[0]
     df.head()
@@ -13,14 +13,18 @@ with open("LLtable.html") as fp:
                 x = i.replace("$", "ending")
                 try:
                     y = r.split("::=")[0]
-                    rule = r.split("::=")[1].split()
-                    if (rule == ["ε"]):
-                        print(f"insertMember({x}, {y}, 0);")
-                        continue
-                    print(f"insertMember({x}, {y}, {len(rule)}", end="")
-                    for l in rule:
-                        l = l.replace("ε","eps").replace("$", "ending")                
-                        print(f", \npartOfRulesRightSide({l})" ,end="")
-                    print(");")
+                    rule = r.split("::=")[1:]
+                    for k in rule:
+                        k = k.split()
+                        if (k[0] == "Exp"):
+                            del k[-1]
+                        if k == ["ε"]:
+                            print(f"insertMember({x}, {y}, 0);")
+                            continue
+                        print(f"insertMember({x}, {y}, {len(k)}", end="")
+                        for l in k:
+                            l = l.replace("ε", "eps").replace("$", "ending")
+                            print(f", {l}", end="")
+                        print(");")
                 except:
                     pass
