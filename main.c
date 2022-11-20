a5df81f67b6ad43c9d2cbb685c8eb99fac460d03
Jan Brudný <jan.brudny@gmail.com>
2022-11-20 14:14:32 +0100

    podle `parser.c` by tohle měl být celý main

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author Jan Brudný (xbrudn02@stud.fit.vut.cz)
+ * @brief Hlavní soubor programu
+ * Implementace překladače imperativního jazyka IFJ21
+ * 
+ * @version 0.1
+ * @date 2022-11-20
+ */
+
+#include "parser.h"
+#include <stdio.h>
+
+int main() {
+    return parser();
+}
