3206cc423e3e68c70a584e9a1d7077ec01358459
Jan Brudný <xbrudn02@stud.fit.vutbr.cz>
2022-11-23 22:41:28 +0100

    zarovnání čísel řádků v debug výstupu

--- common.h
+++ common.h
@@ -34,12 +34,12 @@
 // makra pro logování a easy exity
 //region logginAndExitingMacros
 
-#define InternalError(message, args...)    PrintErrorExit("%15s:%d | in %s() | " message "\n", ERR_RUNTIME ,__FILE__, __LINE__,  __FUNCTION__, ## args)
+#define InternalError(message, args...)    PrintErrorExit("%15s:%-3d | in %s() | " message "\n", ERR_RUNTIME ,__FILE__, __LINE__,  __FUNCTION__, ## args)
 #define PrintErrorExit(format,ERR_CODE,   ...)    do{  fprintf(stderr, format, __VA_ARGS__); fflush(stderr); exit(ERR_CODE);}while(0)
-#define PrettyExit(ERR_CODE)   printlog("%15s:%d | in %s() | Exit with code: %d \n", __FILE__, __LINE__,  __FUNCTION__, ERR_CODE); exit(ERR_CODE)
+#define PrettyExit(ERR_CODE)   printlog("%15s:%-3d | in %s() | Exit with code: %d \n", __FILE__, __LINE__,  __FUNCTION__, ERR_CODE); exit(ERR_CODE)
 
 #define printlog(...)    do{  fprintf(stderr, __VA_ARGS__);}while(0)
-#define loging(message, args...)    if (debug == 1) {printlog("%15s:%d | in %s() | " message "\n", __FILE__, __LINE__,  __FUNCTION__, ## args);}
+#define loging(message, args...)    if (debug == 1) {printlog("%15s:%-3d | in %s() | " message "\n", __FILE__, __LINE__,  __FUNCTION__, ## args);}
 //endregion
 
 
