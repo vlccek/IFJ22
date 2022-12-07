3c8debd32bfbbc64045fd75bf9890d490ea2d107
Antonín Jarolím <jarolim.antonin@seznam.cz>
2022-12-07 12:14:01 +0100

    Checking: Calling undefined function

--- generator_postproces.c
+++ generator_postproces.c
@@ -5,9 +5,6 @@
 #include "generator_postproces.h"
 #include "generator_3adres.h"
 void postProcArrayFunctionParams(i3InstructionArray_t *array, symtable_t *symtable, symbol_t *symbol) {
-    if (strcmp(array->functionName, "$MainBody") == 0) {
-        return;
-    }
     int len = symbol->firstParam->len;
     printf("# len: %d\n", len);
     DTListMem_T *member = symbol->firstParam->first;
@@ -17,19 +14,43 @@ void postProcArrayFunctionParams(i3InstructionArray_t *array, symtable_t *symtab
     }
     // todo vygenerovat $param1 -> $reálný identifikátor
 }
+
+symbol_t *callInsToSymb(i3Instruction_t instruction, symtable_t *symtable) {
+    symbol_t *funcSymbol = symSearchFunc(symtable, instruction.arg1.identifier);
+    if (funcSymbol == NULL) {
+        loging("Calling undefined function!\n");
+        PrettyExit(ERR_FUNCTION_IDENTIFIER);
+    }
+    return funcSymbol;
+}
+void assignFuncReturnTypes(i3InstructionArray_t *array, symtable_t *symtable) {
+    for (int i = 0; i < array->size; ++i) {
+        if (array->instructions[i].type == I_MOVE_RETURN) {
+            symbol_t *funcSymbol = callInsToSymb(array->instructions[i - 1], symtable);
+            array->instructions[i].dest.dataType = funcSymbol->returnType;
+        }
+    }
+}
+
 void postProcArray(i3InstructionArray_t *array, symtable_t *symtable) {
+    // todo: nechceme double DEFVAR instrukce - nastavit na NOOP
+    // todo: vyřeš typové konverze
     symbol_t *symbol = symSearchFunc(symtable, array->functionName);
     if (!symbol) {
         if (strcmp(array->functionName, "$MainBody") != 0) {
             InternalError("Generuju funkci, co není v symtabulce :(");
         }
     }
-    postProcArrayFunctionParams(array, symtable, symbol);
+    if (symbol) {
+        // do not process in main body
+        postProcArrayFunctionParams(array, symtable, symbol);
+    }
+    assignFuncReturnTypes(array, symtable);
 }
 
 void postprocess(i3Table_t program, symtable_t symtable) {
     for (int i = 0; i < MAX_HTSIZE; ++i) {
-        if (&program[i] == NULL)
+        if (program[i].functionName == NULL || program[i].size == 0)
             return;
         postProcArray(&program[i], &symtable);
     }
